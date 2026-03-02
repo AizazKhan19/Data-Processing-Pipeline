99b1b55f90a8950dd85e7125b449d69e679d6f2f
Aizaz Khan <aizazkhan7874@gmail.com>
2026-03-02 22:20:11 +0500

    Datacleaner class with requuired methods complete

--- Datacleaner.py
+++ Datacleaner.py
@@ -13,11 +13,29 @@ class Datacleaner:
         self.df = self.df.drop(columns = [col])
         # print(f' New frame after dropping column :\n {dropped}')
         return self.df
+    
+    # filling numerical data with average/mean of the entire column if an entry has NaN value
+    def mean(self, col):
+        mean_value = self.df[col].mean()
+        self.df[col] = self.df[col].fillna(mean_value)
+        return self.df
+    
+    # filling embarked column vlaues with most frequent values of the column
+    def mode(self, col):
+        mode_value = self.df[col].mode()[0]
+        self.df[col] = self.df[col].fillna(mode_value)
+        return self.df
         
 dcleaner = Datacleaner(df)
 # print(dcleaner.df)
 
 dataframe = dcleaner.dropping('Cabin')
-print(dcleaner.df)
+# print(dataframe)
 
-    
+dataframe = dcleaner.mean('Age')
+# print(dataframe)
+
+dataframe = dcleaner.mode('Embarked')
+print(dataframe)
+
+print(dcleaner.df.isnull().sum())
